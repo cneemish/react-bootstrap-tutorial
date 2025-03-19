dc315dfea902447c47fa4a074dedc2ea37266fc4
cneemish <cneemish@gmail.com>
2025-03-20 00:48:42 +0530

    Adding the app to git

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css'
+import  Button  from 'react-bootstrap/Button'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+
+  return (
+    <>
+      <div>
+       <Button>Test Button</Button>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
