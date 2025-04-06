3aeb124d5199cb4d1e1678b5620fd80e63396bb4
cneemish <cneemish@gmail.com>
2025-04-06 15:17:56 +0530

    Adding Nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,72 @@
 import React from 'react';
-import './App.css'
-import  Button  from 'react-bootstrap/Button'
-
+import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import {
+  Button,
+  Container,
+  Nav,
+  NavDropdown,
+  Navbar,
+  Card, // Import Card component
+  Placeholder, // Import Placeholder component
+} from 'react-bootstrap';
 
 function App() {
-
   return (
-    <>
-      <div>
-       <Button>Test Button</Button>
+    <div> {/* Wrap everything in a div */}
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      <div className="d-flex justify-content-around">
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="holder.js/100px180" />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="holder.js/100px180" />
+          <Card.Body>
+            <Placeholder as={Card.Title} animation="glow">
+              <Placeholder xs={6} />
+            </Placeholder>
+            <Placeholder as={Card.Text} animation="glow">
+              <Placeholder xs={7} /> <Placeholder xs={4} /> <Placeholder xs={4} />{' '}
+              <Placeholder xs={6} /> <Placeholder xs={8} />
+            </Placeholder>
+            <Placeholder.Button variant="primary" xs={6} />
+          </Card.Body>
+        </Card>
       </div>
-      
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
